d046a985d0176bfa5a0dfcc840023a10b4e13de9
Tomas Kukumberg <tomas.kukumberg@centrum.sk>
2023-08-02 12:23:33 +0200

    formatting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -49,15 +49,18 @@ const Header = () => {
             <div className="relative z-10 col-span-3 bg-slate-800 rounded-xl shadow-lg xl:ml-0 dark:shadow-none dark:ring-1 dark:ring-inset dark:ring-white/10 my-4">
                 <div className="relative flex text-slate-400 text-xs leading-6">
                     <div className="mt-2 flex-none text-sky-300 border-t border-b border-t-transparent border-b-sky-300 px-4 py-1 flex items-center">Generated Markdown</div>
-                    <div className="flex-auto flex pt-2 rounded-tr-xl overflow-hidden"><div className="flex-auto -mr-px bg-slate-700/50 border border-slate-500/30 rounded-tl"></div></div>
-                    <div className="absolute top-2 right-0 h-8 flex items-center pr-4"><div className="relative flex -mr-2">
-                        <button type="button" onClick={() => { copyStateToClipboard(code) }} className="text-slate-500 hover:text-slate-400">
-                            <svg fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" className="w-8 h-8">
-                                <path d="M13 10.75h-1.25a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H19"></path>
-                                <path d="M18 12.25h-4a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM13.75 16.25h4.5M13.75 19.25h4.5"></path>
-                            </svg>
-                        </button>
+                    <div className="flex-auto flex pt-2 rounded-tr-xl overflow-hidden">
+                        <div className="flex-auto -mr-px bg-slate-700/50 border border-slate-500/30 rounded-tl"></div>
                     </div>
+                    <div className="absolute top-2 right-0 h-8 flex items-center pr-4">
+                        <div className="relative flex -mr-2">
+                            <button type="button" onClick={() => { copyStateToClipboard(code) }} className="text-slate-500 hover:text-slate-400">
+                                <svg fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" className="w-8 h-8">
+                                    <path d="M13 10.75h-1.25a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H19"></path>
+                                    <path d="M18 12.25h-4a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM13.75 16.25h4.5M13.75 19.25h4.5"></path>
+                                </svg>
+                            </button>
+                        </div>
                     </div>
                 </div>
                 <div className="relative">
@@ -71,7 +74,6 @@ const Header = () => {
                 </div>
             </div>
         </div>
-
     )
 }
 
