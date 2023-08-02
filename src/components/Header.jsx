022a39121be34ba8652829d1d180e06388732344
Tomas Kukumberg <tomas.kukumberg@centrum.sk>
2023-08-02 11:18:41 +0200

    cleanup component with useEffect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 
 const Header = () => {
-    const [rangeValue, setRangeValue] = useState(0);
-    const [text, setText] = useState('');
-    const [code, setCode] = useState('');
-
-    const handleRangeChange = e => setRangeValue(e.target.value);
-    const handleTextChange = e => setText(e.target.value);
-    const copyStateToClipboard = (state) => {navigator.clipboard.writeText(state)};
-    
-    const handleClickMarkdown = () => {
-        setCode(getCode());
-    }
 
     const getHeadingNumber = () => {
         return (rangeValue / 25) + 1;
     }
 
+    const [rangeValue, setRangeValue] = useState(0);
+    const [text, setText] = useState('My heading');
+    const [code, setCode] = useState('#');
+    const headingNumber = getHeadingNumber();
+
+    const handleRangeChange = e => setRangeValue(e.target.value);
+    const handleTextChange = e => setText(e.target.value);
+    const copyStateToClipboard = (state) => {navigator.clipboard.writeText(state)};
+
     const getCode = () => {
-        const headingNumber = getHeadingNumber();
-        return `${"#".repeat(headingNumber)} ${text}`
+        return `${"#".repeat(headingNumber)} ${text}`;
     }
 
-    const handleClick = () => {
+    useEffect(() => {
         setCode(getCode());
-    }
+    }, [rangeValue, text])
 
     return (
         <div className='max-w-[800px] m-auto md:pl-20 p-4 py-16'>
@@ -40,9 +37,8 @@ const Header = () => {
                 <span>H6</span>
             </div>
 
-            <div className="grid md:grid-cols-2 gap-x-4 items-center">
-                <input type="text" placeholder="Text of heading" className="input input-bordered input-primary w-full my-6" value={text} onChange={handleTextChange} />
-                <button className="btn btn-primary" onClick={handleClick}>Generate code</button>
+            <div className='w-full'>
+                <input type="text" placeholder="Text of heading" className="input input-bordered input-primary w-full my-6" value={text} onChange={handleTextChange} autoFocus />
             </div>
 
             <div className="relative z-10 col-span-3 bg-slate-800 rounded-xl shadow-lg xl:ml-0 dark:shadow-none dark:ring-1 dark:ring-inset dark:ring-white/10 my-4">
@@ -50,7 +46,7 @@ const Header = () => {
                     <div className="mt-2 flex-none text-sky-300 border-t border-b border-t-transparent border-b-sky-300 px-4 py-1 flex items-center">Generated Markdown</div>
                     <div className="flex-auto flex pt-2 rounded-tr-xl overflow-hidden"><div className="flex-auto -mr-px bg-slate-700/50 border border-slate-500/30 rounded-tl"></div></div>
                     <div className="absolute top-2 right-0 h-8 flex items-center pr-4"><div className="relative flex -mr-2">
-                        <button type="button" onClick={() => {handleClickMarkdown(); copyStateToClipboard(code)}} className="text-slate-500 hover:text-slate-400">
+                        <button type="button" onClick={() => {copyStateToClipboard(code)}} className="text-slate-500 hover:text-slate-400">
                             <svg fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" className="w-8 h-8">
                                 <path d="M13 10.75h-1.25a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H19"></path>
                                 <path d="M18 12.25h-4a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM13.75 16.25h4.5M13.75 19.25h4.5"></path>
